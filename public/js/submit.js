27bc31194c45f87f6a5258772c86f359fc5fff5a
David Luu <luu.david.c@gmail.com>
2016-02-18 20:14:16 -0800

    Updated submit page; Commented out subcategories

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,5 +1,13 @@
 // jQuery ready ()
 $(function(){
+  var width = $('#previewPlaceholder').css('width');
+  $('#previewPlaceholder').css({
+      'height': width
+  });
+  $('#previewPlaceholder span').css({
+    'font-size': parseInt(width) / 3
+  });
+
   $("#imageInput").change(function(){
     updatePreviewImage(this);
   });
@@ -10,6 +18,10 @@ function updatePreviewImage(input) {
     var reader = new FileReader();
     
     reader.onload = function (e) {
+      $('#previewPlaceholder').css({
+        'display': 'none'
+      });
+      $('#previewImageHolder').removeAttr( 'style' );
       $('#previewImage').attr('src', e.target.result);
     }
     
