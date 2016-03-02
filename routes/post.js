cac35e8e45ae7d0e43a1aaef592f4ee83125502d
David Luu <luu.david.c@gmail.com>
2016-03-02 13:32:13 -0800

    Post comments

--- routes/post.js
+++ routes/post.js
@@ -38,8 +38,6 @@ function renderPage(req, res, pageToRender) {
           'post' : post[0]
         }
 
-        console.log(post);
-
         res.render(pageToRender, info);
       }
     });
@@ -50,16 +48,39 @@ var data = require("./placeholders/post1.json");
 
 exports.post = function(req, res) { 
   var comment = req.body.comment;
-  var url = req.body.url;
 
-  var newPost = {
-    "text" : comment,
-    "upvotes" : 0,
-    "downvotes" : 0
+  var url_base = req.body.base;
+  var url_category = req.body.category;
+  var url_id = req.body.id;
+  var url_title = req.body.title;
 
-  };
+  var url = '/' + url_base + '/' + url_category + '/' + url_id + '/' + url_title;
 
-  data["comments"].push(newPost);
+  models.Post
+    .find({
+      '_id': url_id
+    })
+    .populate('comments')
+    .exec(afterQuery);
+
+  function afterQuery(err, post) {
+    var currPost = post[0];
+
+    var newComment = new models.Comment({
+      "text" : comment,
+      "upvotes" : 0,
+      "downvotes" : 0
+    });
+    newComment.save(function(err, com) {
+      if(err) console.log(err);
+
+      currPost.comments.push(com['_id']);
+
+      currPost.save(function(err) {
+        if(err) console.log(err);
+      })
+    });
+  }
 
   res.redirect(url);
  }
